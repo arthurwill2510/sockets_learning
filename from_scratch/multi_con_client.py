7de0f4c6519ce59adaf44039877dcddd48723dfc
Arthur Williams <arthurwill2510@gmail.com>
2024-01-18 23:53:54 +0000

    This is a correct version of the server and client, better understanding now...

--- from_scratch/multi_con_client.py
+++ from_scratch/multi_con_client.py
@@ -3,44 +3,38 @@ import socket
 import selectors
 
 sel = selectors.DefaultSelector()
-address = ('127.0.0.1', 63000)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.setblocking(False)
-client_socket.connect_ex(address)
-events = selectors.EVENT_READ | selectors.EVENT_WRITE
-data = types.SimpleNamespace(
-    connid=1,
-    msg_total=len(b'TESTING'),
-    recv_total=0,
-    messages=b'TESTING',
-    outb=b"",
-)
-sel.register(client_socket, events, data=data)
+target_address = ('127.0.0.1', 49000)
 
+for connid in range(5):
+    connid += 1
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.setblocking(False)
+    client_socket.connect_ex(target_address)
+    data = types.SimpleNamespace(inb=b'', outb=b'This is the message that I want the server to receive. JUST TESTING', target_addr=target_address, connid=connid)
+    sel.register(client_socket, selectors.EVENT_READ | selectors.EVENT_WRITE, data=data)
 
 while True:
     events = sel.select(timeout=1)
     for event in events:
         key, mask = event
+        sock =key.fileobj
+        data = key.data
 
         if mask & selectors.EVENT_READ:
-            incoming_data = key.fileobj.recv(1024)
-            if incoming_data:
-                print('Data received from' + data.address)
-                data.inb += incoming_data
-                print('Data is:', incoming_data.decode('utf-8'))
-                break
-            else:
-                sel.unregister(key.fileobj)
-                key.fileobj.close()
-                print('The client at' +  data.address + 'has been disconnected')
+            recv_data = sock.recv(1024)
+            if recv_data:
+                data.inb += recv_data
+                print('The client', data.connid ,'has recieved the data')
+                print('Data', data.inb.decode('utf-8'))
 
         if mask & selectors.EVENT_WRITE:
-            outgoing_data = b'HERE 10'
-            amt_bytes_sent = key.fileobj.send(outgoing_data)
-            data.outb += outgoing_data
-            print('Data sent to' , address[0], ':' ,address[1])
-            print('Data', data.outb )
-        
-            
+            if data.inb:
+                amt_bytes_sent = sock.send(data.outb)
+                print('The client', data.connid ,'has sent' , amt_bytes_sent , 'amount of bytes')
+                print('The client', data.connid ,'has been disconnected')
+                sel.unregister(sock)
+                sock.close()
+
+    if not sel.get_map():
+            break
--- from_scratch/multi_con_serv.py
+++ from_scratch/multi_con_serv.py
@@ -1,52 +1,61 @@
 import types
 import socket
-import selectors
+import selectors 
 
-selector = selectors.DefaultSelector()
-address = ('127.0.0.1', 63000)
-
-def accept_wrapper(sock):
-    latest_conn, addr = sock.accept()
-    latest_conn.setblocking(False)
-    data = types.SimpleNamespace(inb=b'', outb=b'', address=addr)
-    selector.register(latest_conn, selectors.EVENT_READ | selectors.EVENT_WRITE, data=data)
-
-def service_client(key, mask):
-    sock = key.fileobj
-    data = key.data
-
-    if mask & selectors.EVENT_READ:
-        incoming_data = sock.recv(1024)
-        if incoming_data:
-            print('Data received from' , data.address[0],':', data.address[1])
-            data.inb += incoming_data
-        else:
-            selector.unregister(sock)
-            sock.close()
-            print('The client at' +  data.address + 'has been disconnected')
-
-    if mask & selectors.EVENT_WRITE:
-        outgoing_data = b'HERE 23'
-        amt_bytes_sent = sock.send(outgoing_data)
-        data.outb += outgoing_data
-        print('Data sent to' , address[0], ':' ,address[1])
-        print('Data', data.outb )
+server_address = ('127.0.0.1', 49000)
+sel = selectors.DefaultSelector()
 
 listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listening_socket.setblocking(False)
-listening_socket.bind(address)
+listening_socket.bind(server_address)
 listening_socket.listen()
-selector.register(listening_socket, selectors.EVENT_READ, data=None)
+sel.register(listening_socket, selectors.EVENT_READ, data=None)
+
+def accept_wrapper(sock):
+    server_end_conn, address = sock.accept()
+    server_end_conn.setblocking(False)
+    print('We have now connected to the server at ', address[0] , ':' , address[1])
+    data = types.SimpleNamespace(inb=b'', outb=b'', addr=address)
+    sel.register(server_end_conn, selectors.EVENT_READ | selectors.EVENT_WRITE, data=data)
+
+def service_socket(key, mask):
+    sock = key.fileobj
+    data = key.data
+
+    if mask & selectors.EVENT_READ: 
+        try:
+            recv_data = sock.recv(1024)
+            if recv_data:
+                print('We recieved Data from the client at', data.addr[0], ':' ,data.addr[1])
+                data.inb += recv_data   
+        except ConnectionResetError:
+            key.fileobj.close()
+            sel.unregister(key.fileobj)
+
+    if mask & selectors.EVENT_WRITE:
+        try:
+            data.outb = b'THIS IS JUST A TEST ROUND TO SEE HOW THINGS WORK NOT TOO SURE ABOUT THIS YET'
+            amt_bytes_sent = sock.send(data.outb) 
+            print(amt_bytes_sent, 'Amount of bytes have been sent over to the client', data.addr[0], ':' ,data.addr[1])
+        except (BrokenPipeError, OSError) as e:
+            if e == BrokenPipeError:
+                key.fileobj.close()
+                sel.unregister(key.fileobj)
+            else:
+                sel.get_map == None
+
+
 
 while True:
-    events = selector.select(timeout=None)
+    events = sel.select(timeout=2)
+    # try:
     for event in events:
         key, mask = event
-        if key.fileobj == listening_socket:
+        if key.data == None:
             accept_wrapper(key.fileobj)
         else:
-            try:
-                service_client(key, mask)
-            except BlockingIOError:
-                # Handle non-blocking operation exception
-                pass
+            service_socket(key, mask)
+        
+    if not events:
+            break
+        
